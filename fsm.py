15367031afeaf5df9ce7dde55572deb3668e533a
FebrinaWijaya <febrinawijaya98@gmail.com>
2018-12-05 22:57:59 +0800

    update fsm

--- fsm.py
+++ fsm.py
@@ -1,14 +1,14 @@
-from transitions.extensions import GraphMachine
-
+#from transitions.extensions import GraphMachine
+ 
 from utils import send_text_message
 
 
 class TocMachine(GraphMachine):
-    def __init__(self, **machine_configs):
-        self.machine = GraphMachine(
-            model=self,
-            **machine_configs
-        )
+    # def __init__(self, **machine_configs):
+    #     self.machine = GraphMachine(
+    #         model=self,
+    #         **machine_configs
+    #     )
 
     def is_going_to_state1(self, event):
         if event.get("message"):

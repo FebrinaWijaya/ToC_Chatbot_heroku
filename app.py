5f2d22ba2122f7db98b42215642b715ecbe3bc35
FebrinaWijaya <febrinawijaya98@gmail.com>
2018-12-08 17:23:50 +0800

    Back to previous working version

--- app.py
+++ app.py
@@ -1,260 +1,46 @@
-#from bottle import route, run, request, abort, static_file
-from bottle import Bottle, route, run, request, abort, static_file
 import os
+from bottle import Bottle, route, request, abort, static_file
+from utils import send_text_message
 from fsm import TocMachine
 
 app = Bottle()
-
+ 
 VERIFY_TOKEN = os.environ['VERIFY_TOKEN']
 PORT = os.environ['PORT']
 
-#VERIFY_TOKEN = "FWy9z9bjutzBb1oLfjt2D" #"Your Webhook Verify Token" 
 machine = TocMachine(
     states=[
-        'init',
-        'purpose',
-        'adventure',
-            'raja_ampat',
-            'gili_island',
-            'mount_bromo',
-            #'mount_rinjani',
-        'historical',
-            'borobudur',
-            'prambanan',
-            'old_town',
-            #'monument_of_people',
-        'naturals',
-            'bali',
-            'lake_toba',
-            'dieng_plateau',
-            #'komodo_nat_park',
-        'local_food',
-            'satay',
-            'rendang',
-            'fried_rice'
-            #'nasi_uduk',
+        'user',
+        'state1',
+        'state2'
     ],
     transitions=[
         {
             'trigger': 'advance',
-            'source': 'init',
-            'dest': 'purpose',
-            'conditions': 'user_sent_something'
-        },
-
-        {
-            'trigger': 'advance',
-            'source': 'purpose',
-            'dest': 'adventure',
-            'conditions': 'is_travelling_for_adventure'
+            'source': 'user',
+            'dest': 'state1',
+            'conditions': 'is_going_to_state1'
         },
         {
             'trigger': 'advance',
-            'source': 'purpose',
-            'dest': 'historical',
-            'conditions': 'is_travelling_for_historical'
+            'source': 'user',
+            'dest': 'state2',
+            'conditions': 'is_going_to_state2'
         },
         {
-            'trigger': 'advance',
-            'source': 'purpose',
-            'dest': 'naturals',
-            'conditions': 'is_travelling_for_naturals'
-        },
-        {
-            'trigger': 'advance',
-            'source': 'purpose',
-            'dest': 'local_food',
-            'conditions': 'is_travelling_for_local_food'
-        },
-        {
-            'trigger': 'advance',
-            'source': 'adventure',
-            'dest': 'raja_ampat',
-            'conditions': 'is_adventure_to_raja_ampat'
-        },
-        {
-            'trigger': 'advance',
-            'source': 'adventure',
-            'dest': 'gili_island',
-            'conditions': 'is_adventure_to_gili_island'
-        },
-        {
-            'trigger': 'advance',
-            'source': 'adventure',
-            'dest': 'mount_bromo',
-            'conditions': 'is_adventure_to_mount_bromo'
-        },
-        {
-            'trigger': 'advance',
-            'source': 'historical',
-            'dest': 'borobudur',
-            'conditions': 'is_historical_to_borobudur'
-        },
-        {
-            'trigger': 'advance',
-            'source': 'historical',
-            'dest': 'prambanan',
-            'conditions': 'is_historical_to_prambanan'
-        },
-        {
-            'trigger': 'advance',
-            'source': 'historical',
-            'dest': 'old_town',
-            'conditions': 'is_historical_to_old_town'
-        },
-        {
-            'trigger': 'advance',
-            'source': 'naturals',
-            'dest': 'bali',
-            'conditions': 'is_naturals_in_bali'
-        },
-        {
-            'trigger': 'advance',
-            'source': 'naturals',
-            'dest': 'lake_toba',
-            'conditions': 'is_naturals_in_lake_toba'
-        },
-        {
-            'trigger': 'advance',
-            'source': 'naturals',
-            'dest': 'dieng_plateau',
-            'conditions': 'is_naturals_in_dieng_plateau'
-        },
-        {
-            'trigger': 'advance',
-            'source': 'local_food',
-            'dest': 'satay',
-            'conditions': 'is_local_food_satay'
-        },
-        {
-            'trigger': 'advance',
-            'source': 'local_food',
-            'dest': 'rendang',
-            'conditions': 'is_local_food_rendang'
-        },
-        {
-            'trigger': 'advance',
-            'source': 'local_food',
-            'dest': 'fried_rice',
-            'conditions': 'is_local_fried_rice'
-        },
-
-        {
-            'trigger': 'advance',
-            'source': 'raja_ampat',
-            'dest': 'gili_island',
-            'conditions': 'is_next'
-        },
-        {
-            'trigger': 'advance',
-            'source': 'gili_island',
-            'dest': 'mount_bromo',
-            'conditions': 'is_next'
-        },
-        {
-            'trigger': 'advance',
-            'source': 'borobudur',
-            'dest': 'prambanan',
-            'conditions': 'is_next'
-        },
-        {
-            'trigger': 'advance',
-            'source': 'prambanan',
-            'dest': 'old_town',
-            'conditions': 'is_next'
-        },
-        {
-            'trigger': 'advance',
-            'source': 'bali',
-            'dest': 'lake_toba',
-            'conditions': 'is_next'
-        },
-        {
-            'trigger': 'advance',
-            'source': 'lake_toba',
-            'dest': 'dieng_plateau',
-            'conditions': 'is_next'
-        },
-        {
-            'trigger': 'advance',
-            'source': 'satay',
-            'dest': 'rendang',
-            'conditions': 'is_next'
-        },
-        {
-            'trigger': 'advance',
-            'source': 'rendang',
-            'dest': 'fried_rice',
-            'conditions': 'is_next'
-        },
-        {
-            'trigger': 'advance',
+            'trigger': 'go_back',
             'source': [
-                'raja_ampat',
-                'gili_island',
-                'mount_bromo',
-                'borobudur',
-                'prambanan',
-                'old_town',
-                'bali',
-                'lake_toba',
-                'dieng_plateau',
-                'satay',
-                'rendang',
-                'fried_rice'
-                ],
-            'dest': 'purpose',
-            'conditions': 'is_change_travel_purpose'
-        },
-        {
-            'trigger': 'advance',
-            'source': [
-                'raja_ampat',
-                'gili_island',
-                'mount_bromo'
-                ],
-            'dest': 'adventure',
-            'conditions': 'is_select_another_place'
-        },
-        {
-            'trigger': 'advance',
-            'source': [
-                'borobudur',
-                'prambanan',
-                'old_town'
-                ],
-            'dest': 'historical',
-            'conditions': 'is_select_another_place'
-        },
-        {
-            'trigger': 'advance',
-            'source': [
-                'bali',
-                'lake_toba',
-                'dieng_plateau'
-                ],
-            'dest': 'naturals',
-            'conditions': 'is_select_another_place'
-        },
-        {
-            'trigger': 'advance',
-            'source': [
-                'satay',
-                'rendang',
-                'fried_rice'
-                ],
-            'dest': 'local_food',
-            'conditions': 'is_select_another_place'
+                'state1',
+                'state2'
+            ],
+            'dest': 'user'
         }
     ],
-    initial='init',
-    auto_transitions=False
-    # ,
-    # show_conditions=True,
+    initial='user',
+    auto_transitions=False,
 )
-#machine.get_graph().draw('fsm.png', prog='dot', format='png')
 
-@route("/webhook", method="GET")
+@app.route("/webhook", method="GET")
 def setup_webhook():
     mode = request.GET.get("hub.mode")
     token = request.GET.get("hub.verify_token")
@@ -267,8 +53,7 @@ def setup_webhook():
     else:
         abort(403)
 
-
-@route("/webhook", method="POST")
+@app.route("/webhook", method="POST")
 def webhook_handler():
     body = request.json
     print('\nFSM STATE: ' + machine.state)
@@ -279,14 +64,18 @@ def webhook_handler():
         event = body['entry'][0]['messaging'][0]
         machine.advance(event)
         return 'OK'
+    # body = request.json
+    # print('REQUEST BODY: ')
+    # print(body)
 
-
-@route('/show-fsm', methods=['GET'])
-def show_fsm():
-    machine.get_graph().draw('fsm.png', prog='dot', format='png')
-    return static_file('fsm.png', root='./', mimetype='image/png')
+    # if body['object'] == "page":
+    #     event = body['entry'][0]['messaging'][0]
+    #     if event.get("message"):
+    #         text = event['message']['text']
+    #         sender_id = event['sender']['id']
+    #         send_text_message(sender_id, text)
+    #     return 'OK'
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=PORT, debug=True, reloader=True)
-
--- fsm.py
+++ fsm.py
@@ -1,9 +1,7 @@
 from transitions import Machine
-
+ 
 from utils import send_text_message
-from utils import send_image_url
-from utils import send_button_message
-from data import data
+
 
 class TocMachine(Machine):
     def __init__(self, **machine_configs):
@@ -11,331 +9,35 @@ class TocMachine(Machine):
             model=self,
             **machine_configs
         )
-    def user_sent_something(self, event):
-        return True
 
-    def is_travelling_for_adventure(self, event):
+    def is_going_to_state1(self, event):
         if event.get("message"):
             text = event['message']['text']
-            return text.lower() == 'adventure' or text.lower() == '1'
-        elif (event.get("postback")):
-            text = event['postback']['payload']
-            return text.lower() == 'adventure' or text.lower() == '1'
+            return text.lower() == 'go to state1'
         return False
 
-    def is_travelling_for_historical(self, event):
+    def is_going_to_state2(self, event):
         if event.get("message"):
             text = event['message']['text']
-            return text.lower() == 'historical buildings'  or text.lower() == '2'
-        elif (event.get("postback")):
-            text = event['postback']['payload']
-            return text.lower() == 'historical buildings' or text.lower() == '2'
+            return text.lower() == 'go to state2'
         return False
 
-    def is_travelling_for_naturals(self, event):
-        if event.get("message"):
-            text = event['message']['text']
-            return text.lower() == 'naturals' or text.lower() == '3'
-        elif (event.get("postback")):
-            text = event['postback']['payload']
-            return text.lower() == 'naturals'  or text.lower() == '3'
-        return False
-
-    def is_travelling_for_local_food(self, event):
-        if event.get("message"):
-            text = event['message']['text']
-            return text.lower() == 'local food' or text.lower() == '4'
-        elif (event.get("postback")):
-            text = event['postback']['payload']
-            return text.lower() == 'local food' or text.lower() == '4'
-        return False
-
-    def on_enter_purpose(self, event):
-        print("I'm entering purpose")
+    def on_enter_state1(self, event):
+        print("I'm entering state1")
 
         sender_id = event['sender']['id']
-        responese = send_text_message(sender_id, data['text'])
-        response = send_image_url(sender_id, "./fsm.png", 'png')
-        # send_button_message(sender_id, "select one", data['buttons'])
+        responese = send_text_message(sender_id, "I'm entering state1")
+        self.go_back()
 
-    # def on_exit_purpose(self):
-    #     print('Leaving purpose')
+    def on_exit_state1(self):
+        print('Leaving state1')
 
-    def on_enter_adventure(self, event):
-        print("I'm entering adventure")
-
-        sender_id = event['sender']['id']
-        send_text_message(sender_id, data['Adventure']['text_to_send'])
-        send_button_message(sender_id, data['Adventure']['text_button'], data['Adventure']['buttons'])
-
-    # def on_exit_adventure(self):
-    #     print('Leaving adventure')
-
-    def on_enter_historical(self, event):
-        print("I'm entering historical")
+    def on_enter_state2(self, event):
+        print("I'm entering state2")
 
         sender_id = event['sender']['id']
-        send_text_message(sender_id, data['Historical Buildings']['text_to_send'])
-        send_button_message(sender_id, data['Historical Buildings']['text_button'], data['Historical Buildings']['buttons'])
+        send_text_message(sender_id, "I'm entering state2")
+        self.go_back()
 
-    # def on_exit_historical(self):
-    #     print('Leaving historical')
-
-    def on_enter_naturals(self, event):
-        print("I'm entering naturals")
-
-        sender_id = event['sender']['id']
-        send_text_message(sender_id, data['Naturals']['text_to_send'])
-        send_button_message(sender_id, data['Naturals']['text_button'], data['Naturals']['buttons'])
-
-    # def on_exit_naturals(self):
-    #     print('Leaving naturals')
-
-    def on_enter_local_food(self, event):
-        print("I'm entering local food")
-
-        sender_id = event['sender']['id']
-        send_text_message(sender_id, data['Local Food']['text_to_send'])
-        send_button_message(sender_id, data['Local Food']['text_button'], data['Local Food']['buttons'])
-
-    # def on_exit_local_food(self):
-    #     print('Leaving local food')
-#########################################################################
-
-    def is_adventure_to_raja_ampat(self, event):
-        if event.get("message"):
-            text = event['message']['text']
-            return text.lower() == 'raja ampat islands, west papua'
-        elif (event.get("postback")):
-            text = event['postback']['payload']
-            return text.lower() == 'raja ampat islands, west papua'
-        return False
-
-    def is_adventure_to_gili_island(self, event):
-        if event.get("message"):
-            text = event['message']['text']
-            return text.lower() == 'gili islands, lombok'
-        elif (event.get("postback")):
-            text = event['postback']['payload']
-            return text.lower() == 'gili islands, lombok'
-        return False
-
-    def is_adventure_to_mount_bromo(self, event):
-        if event.get("message"):
-            text = event['message']['text']
-            return text.lower() == 'mount bromo, east java'
-        elif (event.get("postback")):
-            text = event['postback']['payload']
-            return text.lower() == 'mount bromo, east java'
-        return False
-
-    def on_enter_raja_ampat(self, event):
-        print("I'm entering raja ampat")
-
-        sender_id = event['sender']['id']
-        send_text_message(sender_id, data['Adventure']['Raja Ampat Islands, West Papua']['text'])
-        send_image_url(sender_id, data['Adventure']['Raja Ampat Islands, West Papua']['image'], data['Adventure']['Raja Ampat Islands, West Papua']['image_type'])
-        send_button_message(sender_id, "quick options:", data['Adventure']['Raja Ampat Islands, West Papua']['buttons'])
-
-    def on_enter_gili_island(self, event):
-        print("I'm entering gili island")
-
-        sender_id = event['sender']['id']
-        send_text_message(sender_id, data['Adventure']['Gili Islands, Lombok']['text'])
-        send_image_url(sender_id, data['Adventure']['Gili Islands, Lombok']['image'], data['Adventure']['Gili Islands, Lombok']['image_type'])
-        send_button_message(sender_id, "quick options:", data['Adventure']['Gili Islands, Lombok']['buttons'])
-
-    def on_enter_mount_bromo(self, event):
-        print("I'm entering mount_bromo")
-
-        sender_id = event['sender']['id']
-        send_text_message(sender_id, data['Adventure']['Mount Bromo, East Java']['text'])
-        send_image_url(sender_id, data['Adventure']['Mount Bromo, East Java']['image'], data['Adventure']['Mount Bromo, East Java']['image_type'])
-        send_button_message(sender_id, "quick options:", data['Adventure']['Mount Bromo, East Java']['buttons'])
-
-#########################################################################
-
-    def is_historical_to_borobudur(self, event):
-        if event.get("message"):
-            text = event['message']['text']
-            return text.lower() == 'borobudur temple, magelang'
-        elif (event.get("postback")):
-            text = event['postback']['payload']
-            return text.lower() == 'borobudur temple, magelang'
-        return False
-
-    def is_historical_to_prambanan(self, event):
-        if event.get("message"):
-            text = event['message']['text']
-            return text.lower() == 'prambanan temple, yogyakarta'
-        elif (event.get("postback")):
-            text = event['postback']['payload']
-            return text.lower() == 'prambanan temple, yogyakarta'
-        return False
-
-    def is_historical_to_old_town(self, event):
-        if event.get("message"):
-            text = event['message']['text']
-            return text.lower() == 'kota tua (old town), jakarta'
-        elif (event.get("postback")):
-            text = event['postback']['payload']
-            return text.lower() == 'kota tua (old town), jakarta'
-        return False
-
-    def on_enter_borobudur(self, event):
-        print("I'm entering borobudur")
-
-        sender_id = event['sender']['id']
-        send_text_message(sender_id, data['Historical Buildings']['Borobudur Temple, Magelang']['text'])
-        send_image_url(sender_id, data['Historical Buildings']['Borobudur Temple, Magelang']['image'], data['Historical Buildings']['Borobudur Temple, Magelang']['image_type'])
-        send_button_message(sender_id, "quick options:", data['Historical Buildings']['Borobudur Temple, Magelang']['buttons'])
-
-    def on_enter_prambanan(self, event):
-        print("I'm entering prambanan")
-
-        sender_id = event['sender']['id']
-        send_text_message(sender_id, data['Historical Buildings']['Prambanan Temple, Yogyakarta']['text'])
-        send_image_url(sender_id, data['Historical Buildings']['Prambanan Temple, Yogyakarta']['image'], data['Historical Buildings']['Prambanan Temple, Yogyakarta']['image_type'])
-        send_button_message(sender_id, "quick options:", data['Historical Buildings']['Prambanan Temple, Yogyakarta']['buttons'])
-
-    def on_enter_old_town(self, event):
-        print("I'm entering old town")
-
-        sender_id = event['sender']['id']
-        send_text_message(sender_id, data['Historical Buildings']['Kota Tua (Old Town), Jakarta']['text'])
-        send_image_url(sender_id, data['Historical Buildings']['Kota Tua (Old Town), Jakarta']['image'], data['Historical Buildings']['Kota Tua (Old Town), Jakarta']['image_type'])
-        send_button_message(sender_id, "quick options:", data['Historical Buildings']['Kota Tua (Old Town), Jakarta']['buttons'])
-
-#########################################################################
-
-    def is_naturals_in_bali(self, event):
-        if event.get("message"):
-            text = event['message']['text']
-            return text.lower() == 'bali, the island of gods'
-        elif (event.get("postback")):
-            text = event['postback']['payload']
-            return text.lower() == 'bali, the island of gods'
-        return False
-
-    def is_naturals_in_lake_toba(self, event):
-        if event.get("message"):
-            text = event['message']['text']
-            return text.lower() == 'lake toba, north sumatra'
-        elif (event.get("postback")):
-            text = event['postback']['payload']
-            return text.lower() == 'lake toba, north sumatra'
-        return False
-
-    def is_naturals_in_dieng_plateau(self, event):
-        if event.get("message"):
-            text = event['message']['text']
-            return text.lower() == 'dieng plateau'
-        elif (event.get("postback")):
-            text = event['postback']['payload']
-            return text.lower() == 'dieng plateau'
-        return False
-
-    def on_enter_bali(self, event):
-        print("I'm entering bali")
-
-        sender_id = event['sender']['id']
-        send_text_message(sender_id, data['Naturals']['Bali, the Island of Gods']['text'])
-        send_image_url(sender_id, data['Naturals']['Bali, the Island of Gods']['image'], data['Naturals']['Bali, the Island of Gods']['image_type'])
-        send_button_message(sender_id, "quick options:", data['Naturals']['Bali, the Island of Gods']['buttons'])
-
-    def on_enter_lake_toba(self, event):
-        print("I'm entering lake toba")
-
-        sender_id = event['sender']['id']
-        send_text_message(sender_id, data['Naturals']['Lake Toba, North Sumatra']['text'])
-        send_image_url(sender_id, data['Naturals']['Lake Toba, North Sumatra']['image'], data['Naturals']['Lake Toba, North Sumatra']['image_type'])
-        send_button_message(sender_id, "quick options:", data['Naturals']['Lake Toba, North Sumatra']['buttons'])
-
-    def on_enter_dieng_plateau(self, event):
-        print("I'm entering dieng plateau")
-
-        sender_id = event['sender']['id']
-        send_text_message(sender_id, data['Naturals']['Dieng Plateau']['text'])
-        send_image_url(sender_id, data['Naturals']['Dieng Plateau']['image'], data['Naturals']['Dieng Plateau']['image_type'])
-        send_button_message(sender_id, "quick options:", data['Naturals']['Dieng Plateau']['buttons'])
-
-#########################################################################
-
-    def is_local_food_satay(self, event):
-        if event.get("message"):
-            text = event['message']['text']
-            return text.lower() == 'satay'
-        elif (event.get("postback")):
-            text = event['postback']['payload']
-            return text.lower() == 'satay'
-        return False
-
-    def is_local_food_rendang(self, event):
-        if event.get("message"):
-            text = event['message']['text']
-            return text.lower() == 'rendang'
-        elif (event.get("postback")):
-            text = event['postback']['payload']
-            return text.lower() == 'rendang'
-        return False
-
-    def is_local_food_fried_rice(self, event):
-        if event.get("message"):
-            text = event['message']['text']
-            return text.lower() == 'fried rice'
-        elif (event.get("postback")):
-            text = event['postback']['payload']
-            return text.lower() == 'fried rice'
-        return False
-
-    def on_enter_satay(self, event):
-        print("I'm entering satay")
-
-        sender_id = event['sender']['id']
-        send_text_message(sender_id, data['Local Food']['Satay']['text'])
-        send_image_url(sender_id, data['Local Food']['Satay']['image'], data['Local Food']['Satay']['image_type'])
-        send_button_message(sender_id, "quick options:", data['Local Food']['Satay']['buttons'])
-
-    def on_enter_rendang(self, event):
-        print("I'm entering rendang")
-
-        sender_id = event['sender']['id']
-        send_text_message(sender_id, data['Local Food']['Rendang']['text'])
-        send_image_url(sender_id, data['Local Food']['Rendang']['image'], data['Local Food']['Rendang']['image_type'])
-        send_button_message(sender_id, "quick options:", data['Local Food']['Rendang']['buttons'])
-
-    def on_enter_fried_rice(self, event):
-        print("I'm entering fried rice")
-
-        sender_id = event['sender']['id']
-        send_text_message(sender_id, data['Local Food']['Fried Rice']['text'])
-        send_image_url(sender_id, data['Local Food']['Fried Rice']['image'], data['Local Food']['Fried Rice']['image_type'])
-        send_button_message(sender_id, "quick options:", data['Local Food']['Fried Rice']['buttons'])
-
-############################################################
-    def is_next(self, event):
-        if event.get("message"):
-            text = event['message']['text']
-            return text.lower() == 'see next recommended place'
-        elif (event.get("postback")):
-            text = event['postback']['payload']
-            return text.lower() == 'see next recommended place'
-        return False
-
-    def is_select_another_place(self, event):
-        if event.get("message"):
-            text = event['message']['text']
-            return text.lower() == 'select another place'
-        elif (event.get("postback")):
-            text = event['postback']['payload']
-            return text.lower() == 'select another place'
-        return False
-
-    def is_change_travel_purpose(self, event):
-        if event.get("message"):
-            text = event['message']['text']
-            return text.lower() == 'change my travel purpose'
-        elif (event.get("postback")):
-            text = event['postback']['payload']
-            return text.lower() == 'change my travel purpose'
-        return False
+    def on_exit_state2(self):
+        print('Leaving state2')
